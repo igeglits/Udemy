2b42a9c7941272fbfc4c0e6e45515502903c831f
Игорь Эглит <igeglits@gmail.com>
2023-10-18 13:14:11 +0300

    001 Операция условного перехода if if else

---
+++ src/main/java/com/example/section02_structure/IfElse.java
@@ -0,0 +1,33 @@
+package com.example.section02_structure;
+
+import java.util.Scanner;
+
+public class IfElse {
+    public static void main(String[] args) {
+        var a = new Scanner(System.in).nextInt();
+        var b = new Scanner(System.in).nextInt();
+        if (a > b) {
+            var result = "a>b";
+            System.out.println(result);
+        }
+        //
+        if (a > b) {
+            System.out.println("a>b");
+        } else if (a < b) {
+            System.out.println("a<b");
+        } else {
+            System.out.println("a=b");
+        }
+//другой вариант
+        boolean condition = a > b;
+        if (condition) {
+            System.out.println("condition=true");
+        }
+        //
+        if(condition || a<b && a>1 || !(b<7)){
+            System.out.println("complex condition");
+        }
+    }
+
+    }
+
