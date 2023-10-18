a8e6db13ea33f0e5250cb79329f318f7c13c6dff
Игорь Эглит <igeglits@gmail.com>
2023-10-18 12:09:19 +0300

    переменные, инкременты декременты

--- src/main/java/com/example/section02_structure/PrefixPostfixVersions.java
+++ src/main/java/com/example/section02_structure/PrefixPostfixVersions.java
@@ -2,6 +2,19 @@ package com.example.section02_structure;
 
 public class PrefixPostfixVersions {
     public static void main(String[] args) {
+        var a = 0;
+        ++a;
+        a++;
+        System.out.println(a);
 
+        var b = a++;//var b = a; a = a+1; сначала а присвоилась к б (2), потом а увеличилась на 1 и стала (3)
+        System.out.println();
+        System.out.println(b);
+        System.out.println(a);
+
+        var c = ++a;//a = a + 1; var c = a; сначала а увеличилась на 1 потом сумма присвоилась к c
+        System.out.println();
+        System.out.println(a);
+        System.out.println(c);
     }
 }
