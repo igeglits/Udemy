7279f3fc7c094107ecf57f9a1816f8aee68b8c9c
Игорь Эглит <igeglits@gmail.com>
2023-10-19 15:24:05 +0300

    var result = a > b ? a : b;тернарный оператор

---
+++ src/main/java/com/example/section02_structure/DisplayMaxNumber.java
@@ -0,0 +1,11 @@
+package com.example.section02_structure;
+
+public class DisplayMaxNumber {
+    public static void main(String[] args) {
+        var a = 8;
+        var b = 6;
+
+        var result = a > b ? a : b;
+        System.out.println(result);
+    }
+}
