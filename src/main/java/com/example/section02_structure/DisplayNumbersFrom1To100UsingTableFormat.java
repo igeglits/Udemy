590f406339065b1392498c9fb74b9d334867ab13
Игорь Эглит <igeglits@gmail.com>
2023-10-25 20:18:24 +0300

    var from = 1;         var to = 100;         var cols = 10;         for (var i = from; i <= to; i++) {             System.out.print(i + "\t ");             if (i % cols == 0) {                 System.out.println();

---
+++ src/main/java/com/example/section02_structure/DisplayNumbersFrom1To100UsingTableFormat.java
@@ -0,0 +1,15 @@
+package com.example.section02_structure;
+
+public class DisplayNumbersFrom1To100UsingTableFormat {
+    public static void main(String[] args) {
+        var from = 1;
+        var to = 100;
+        var cols = 10;
+        for (var i = from; i <= to; i++) {
+            System.out.print(i + "\t ");
+            if (i % cols == 0) {
+                System.out.println();
+            }
+        }
+    }
+}
