2184e47642a5891bb08bca0ed589e3e0e78c7293
Игорь Эглит <igeglits@gmail.com>
2023-10-25 20:25:51 +0300

    while int i = 0;             while (i < 5) {                 System.out.print(i++ + " ");             }             System.out.println();

---
+++ src/main/java/com/example/section02_structure/While.java
@@ -0,0 +1,24 @@
+package com.example.section02_structure;
+
+public class While {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        {
+            int i = 0;
+            for (; i < 5; ) {
+                System.out.print(i++ + " ");
+            }
+            System.out.println();
+        }
+        {
+            int i = 0;
+            while (i < 5) {
+                System.out.print(i++ + " ");
+            }
+            System.out.println();
+        }
+    }
+}
