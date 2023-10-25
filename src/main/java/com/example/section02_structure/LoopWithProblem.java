790cb976aedd0c363cfdb8b5074ead8f2ae56b2d
Игорь Эглит <igeglits@gmail.com>
2023-10-25 19:55:21 +0300

    for

---
+++ src/main/java/com/example/section02_structure/LoopWithProblem.java
@@ -0,0 +1,10 @@
+package com.example.section02_structure;
+
+public class LoopWithProblem {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            //--i;
+        }
+        System.out.println("end");
+    }
+}
---
+++ src/main/java/com/example/section02_structure/WhichProblemDoCyclesSolve.java
@@ -0,0 +1,10 @@
+package com.example.section02_structure;
+
+public class WhichProblemDoCyclesSolve {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Hello World");
+        }
+    }
+
+}
