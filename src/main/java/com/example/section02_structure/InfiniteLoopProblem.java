63a674fe0ff2e2eb7eb504103be1c11416014711
Игорь Эглит <igeglits@gmail.com>
2023-10-26 13:54:57 +0300

    бесконечные циклы

---
+++ src/main/java/com/example/section02_structure/InfiniteLoopProblem.java
@@ -0,0 +1,21 @@
+package com.example.section02_structure;
+
+public class InfiniteLoopProblem {
+    public static void main(String[] args) {
+       /* for (; ; ) {
+            System.out.println("Hello World");
+        }
+        for (; true; ) {
+            System.out.println("Hello World");
+        }
+        for (; 2 > 1; ) {
+            System.out.println("Hello World");
+        }
+        while (true) {
+        }
+        do {}; while (true);
+        */
+        }
+    }
+
+
