2ecafae48cd42cb518f3c1e0083b939d7463f7e5
Игорь Эглит <igeglits@gmail.com>
2023-10-26 11:40:12 +0300

    do while

--- src/main/java/com/example/section02_structure/DisplayEvenNumbersFrom0To50.java
+++ src/main/java/com/example/section02_structure/DisplayEvenNumbersFrom0To50.java
@@ -12,8 +12,18 @@ public class DisplayEvenNumbersFrom0To50 {
             int i = 0;
             while (i <= 50) {
                 System.out.print(i + " ");
-                i+=2;
+                i += 2;
             }
+            System.out.println();
+        }
+        {
+            int i = 0;
+            do {
+                System.out.print(i + " ");
+                i += 2;
+            }
+            while (i <= 50);
         }
     }
 }
+
--- src/main/java/com/example/section02_structure/DisplayNumbersFrom100To1.java
+++ src/main/java/com/example/section02_structure/DisplayNumbersFrom100To1.java
@@ -2,17 +2,26 @@ package com.example.section02_structure;
 
 public class DisplayNumbersFrom100To1 {
     public static void main(String[] args) {
-        var from =100;
-        var to =1;
-        var step=1;
-        for (var i=from; i>=to; i-=step){
-            System.out.println(i);
+        var from = 100;
+        var to = 1;
+        var step = 1;
+        for (var i = from; i >= to; i -= step) {
+            System.out.print(i+" ");
         }
+        System.out.println();
         {
             int i = 100;
-            while(i>=1){
+            while (i >= 1) {
                 System.out.print(i-- + " ");
             }
+            System.out.println();
         }
+            {
+                int i = 100;
+                do {
+                    System.out.print(i-- + " ");
+                } while (i >= 1);
+            }
+
     }
 }
