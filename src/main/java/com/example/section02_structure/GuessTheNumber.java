3cca8a32994b36468c5fb9cac9a53acdf3016b3a
Игорь Эглит <igeglits@gmail.com>
2023-10-27 15:10:16 +0300

    бесконечные циклы угадай число

--- src/main/java/com/example/section02_structure/GuessTheNumber.java
+++ src/main/java/com/example/section02_structure/GuessTheNumber.java
@@ -5,30 +5,42 @@ import java.util.Scanner;
 
 public class GuessTheNumber {
     public static void main(String[] args) {
-        var number = new Random().nextInt(10);
-        //System.out.println(number);
-        System.out.println("Enter number 0-9:");
-        var guess = new Scanner(System.in).nextInt();
-        if (guess == number) {
-            System.out.println("Congratulations, you guessed the number!");
-        } else {
-            System.out.println("Sorry, but your number is invalid! Try again later...");
-        }//второй вариант
+        while (true) {
 
-        String result;
-        if (guess == number) {
-            result = "Congratulations, you guessed the number!";
-        } else {
-            result = "Sorry, but your number is invalid! Try again later...";
+            var number = new Random().nextInt(10);
+            //System.out.println(number);
+            System.out.println("Enter number 0-9:");
+            var guess = new Scanner(System.in).nextInt();
+            if (guess == number) {
+                System.out.println("Congratulations, you guessed the number! " + number);
+            } else if (guess == 10) {
+                break;
+            } else if (guess < 0 || guess >= 11) {
+                System.out.println("Sorry, but your number is invalid! Try again later...");
+            } else if (guess > number) {
+                System.out.println("Твое число больше, пробуй еще раз:");
+            } else if (guess < number) {
+                System.out.println("Твое число меньше, пробуй еще раз: ");
+            }
+            //второй вариант
+
+            /*String result;
+            if (guess == number) {
+                result = "Congratulations, you guessed the number!";
+            } else {
+                result = "Sorry, but your number is invalid! Try again later...";
+            }
+            System.out.println(result);
+
+            // третий вариант
+            System.out.println(guess == number ? "Congratulations, you guessed the number!" : "Sorry, but your number is invalid! Try again later...");
+            // или
+            String result2 = guess == number ?
+                    "Congratulations, you guessed the number!" :
+                    "Sorry, but your number is invalid! Try again later...";
+            System.out.println(result2);*/
         }
-        System.out.println(result);
-
-        // третий вариант
-        System.out.println(guess == number ? "Congratulations, you guessed the number!" : "Sorry, but your number is invalid! Try again later...");
-        // или
-        String result2 = guess == number ?
-                "Congratulations, you guessed the number!" :
-                "Sorry, but your number is invalid! Try again later...";
-        System.out.println(result2);
+        System.out.println("end");
     }
+
 }
