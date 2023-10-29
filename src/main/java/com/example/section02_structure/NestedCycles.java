b8f72219b86bb9596fbc4263cbfe95d84704e2a8
Игорь Эглит <igeglits@gmail.com>
2023-10-29 14:25:10 +0200

    for (int i = 0; i < 10; i++) {             for (int j = 0; j < 10; j++)

---
+++ src/main/java/com/example/section02_structure/NestedCycles.java
@@ -0,0 +1,12 @@
+package com.example.section02_structure;
+
+public class NestedCycles {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.print((i * 10 + j + 1) + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
