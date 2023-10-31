cea87ec70f0073a5ec5ba849c6dc01e742ab77f3
Игорь Эглит <igeglits@gmail.com>
2023-10-31 14:42:14 +0200

    массив array

---
+++ src/main/java/com/example/section02_structure/WhatIsArray.java
@@ -0,0 +1,10 @@
+package com.example.section02_structure;
+
+public class WhatIsArray {
+    public static void main(String[] args) {
+
+
+        int[] array = {1, 2, 3, 4, 5};
+        System.out.println(array);
+    }
+}
