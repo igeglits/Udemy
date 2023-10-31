2c21d605d929e342ab42670917f5a2263e8b337a
Игорь Эглит <igeglits@gmail.com>
2023-10-31 14:45:16 +0200

    массив типы данных

---
+++ src/main/java/com/example/section02_structure/NewArray.java
@@ -0,0 +1,11 @@
+package com.example.section02_structure;
+
+public class NewArray {
+    public static void main(String[] args) {
+        int[] arrayWithValues1 = {1, 2, 3, 4, 5};
+        double[] arrayWithValues2 = {1., 2., 3., 4., 5.};
+        boolean[] arrayWithValues3 = {true, false, true, false, true};
+        char[] arrayWithValues4 = {'1', '2', '3', '4', '5'};
+        String[] arrayWithValues5 = {"1", "2", "3", "4", "5"};
+    }
+}
