b56b14e128331eb88b92af173910f5b4b1e3f240
Игорь Эглит <igeglits@gmail.com>
2023-11-08 14:08:02 +0200

    _ Вывод строки в обратном порядке.

--- src/main/java/com/example/section02_structure/PrintReverseString.java
+++ src/main/java/com/example/section02_structure/PrintReverseString.java
@@ -6,6 +6,7 @@ public class PrintReverseString {
     public static void main(String[] args) {
         String s ="!dlrow olleH";
         for (int i = s.length()-1; i >=0; i--) {//i = s.length()-1; i >=0; i-- т.к. выводим в обратном порядке
+            //длина строки = 12 (от единицы), а индекс самый большой = 11 (от нуля), потому s.length()-1
             System.out.print(s.charAt(i));
         }
         System.out.println();
