84eafd880b66dbe39a7fa0639fe00b954fe4d473
Игорь Эглит <igeglits@gmail.com>
2023-11-16 14:20:30 +0200

    локальные и блок переменных

---
+++ src/main/java/com/example/section02_structure/BlockExample.java
@@ -0,0 +1,27 @@
+
+package com.example.section02_structure;
+
+/**
+ * @author devonline
+ * @link http://devonline.academy/java-basic
+ */
+public class BlockExample {
+
+    public static void main(String[] args) {
+        int a = 0;
+        {
+            int b = 0;
+            System.out.println(a);
+            System.out.println(b);
+            {
+                int c = 0;
+                System.out.println(a);
+                System.out.println(b);
+                System.out.println(c);
+            }
+            System.out.println("");
+        }
+
+        System.out.println(a);
+    }
+}
---
+++ src/main/java/com/example/section02_structure/LocalVars.java
@@ -0,0 +1,13 @@
+package com.example.section02_structure;
+
+public class LocalVars {
+    public static void main(String[] args) {
+        int a = 2;
+    }
+    private static void function(){
+        int a = 2;
+    }
+    private static void function2(int a){
+
+    }
+}
