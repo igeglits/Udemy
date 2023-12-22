c0abf72483ce1a285bcda034a302323f10893e65
Igor Eglit <igeglits@gmail.com>
2023-12-22 20:39:07 +0200

    рекурсивные функции. index

--- src/main/java/com/example/section02_structure/LinearSearch.java
+++ src/main/java/com/example/section02_structure/LinearSearch.java
@@ -3,15 +3,49 @@ package com.example.section02_structure;
 public class LinearSearch {
     public static void main(String[] args) {
         int[] array = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-        var query = 60;//если данный элемент в массиве и каков его индекс
-
+        var query = 90;//если данный элемент в массиве и каков его индекс
         var index = -1;//задаем индикатор для случая, когда такого элемента нет в массиве
+        LinearSearch linearSearch = new LinearSearch();
+
+        //index = iterativeIndex(array, query, index);
+        System.out.println(iterativeIndex(array, query, index));//выводим индикатор
+
+        //index = linearSearch.recursionIndex(array, query, 0);
+        System.out.println(linearSearch.recursionIndex(array, query, 0));
+
+        //index = linearSearch.recursionIndex2(array, query, array.length - 1, 0);
+        System.out.println(linearSearch.recursionIndex2(array, query, array.length - 1, 0));
+    }
+
+    public static int iterativeIndex(int[] array, int query, int index) {
         for (int i = 0; i < array.length; i++) {
             if (array[i] == query) {
                 index = i;//индикатор получает индекс соответствующий индексу элемента
                 break;
             }
         }
-        System.out.println(index);//выводим индикатор
+        return index;
+    }
+
+    public int recursionIndex(int[] array, int query, int index) {
+        if (index >= array.length) {
+            return -1;
+        } else if
+        (array[index] == query) {
+            return array[index];
+        } else {
+            return recursionIndex(array, query, index + 1);
+        }
+    }
+
+    public int recursionIndex2(int[] array,
+                               int query,
+                               int index, int i) {
+        if (index < 0) {
+            return -1;
+        } else {
+            index = recursionIndex2(array, query, (array.length - ++i),i);
+            return (array[index] == query) ? index : recursionIndex2(array, query, (array.length - 1),i);
+        }
     }
 }
