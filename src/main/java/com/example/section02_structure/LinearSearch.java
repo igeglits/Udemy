1b3540439f5ac1afe29c9b9083787d5ae222826f
Igor Eglit <igeglits@gmail.com>
2023-12-23 20:23:40 +0200

    рекурсивные функции. минимальные .тест.

--- src/main/java/com/example/section02_structure/LinearSearch.java
+++ src/main/java/com/example/section02_structure/LinearSearch.java
@@ -3,7 +3,7 @@ package com.example.section02_structure;
 public class LinearSearch {
     public static void main(String[] args) {
         int[] array = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-        var query = 90;//если данный элемент в массиве и каков его индекс
+        var query = 9;//если данный элемент в массиве и каков его индекс
         var index = -1;//задаем индикатор для случая, когда такого элемента нет в массиве
         LinearSearch linearSearch = new LinearSearch();
 
@@ -14,7 +14,7 @@ public class LinearSearch {
         System.out.println(linearSearch.recursionIndex(array, query, 0));
 
         //index = linearSearch.recursionIndex2(array, query, array.length - 1, 0);
-        System.out.println(linearSearch.recursionIndex2(array, query, array.length - 1, 0));
+        System.out.println(linearSearch.recursionIndexBadExample(array, query, 0));
     }
 
     public static int iterativeIndex(int[] array, int query, int index) {
@@ -32,20 +32,18 @@ public class LinearSearch {
             return -1;
         } else if
         (array[index] == query) {
-            return array[index];
+            return index;
         } else {
             return recursionIndex(array, query, index + 1);
         }
     }
 
-    public int recursionIndex2(int[] array,
-                               int query,
-                               int index, int i) {
-        if (index < 0) {
+    public int recursionIndexBadExample(int[] array, int query, int index) {
+        if (index >= array.length) {
             return -1;
         } else {
-            index = recursionIndex2(array, query, (array.length - ++i),i);
-            return (array[index] == query) ? index : recursionIndex2(array, query, (array.length - 1),i);
+           var foundIndex = recursionIndexBadExample(array, query, index +1);
+            return (array[index] == query) ? index : foundIndex;
         }
     }
 }
